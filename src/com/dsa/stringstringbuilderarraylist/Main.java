2405bcd292d6ebe2d22de8d9d400eef941856796
Manav Jain <manavj0120@gmail.com>
2022-03-03 22:49:35 +0530

    PalindromicSubstrings

---
+++ src/com/dsa/stringstringbuilderarraylist/Main.java
@@ -0,0 +1,11 @@
+package com.dsa.stringstringbuilderarraylist;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        String s = scn.nextLine();
+        PalindromicSubstrings.solution(s);
+    }
+}
--- src/com/dsa/twodarrays/Basic.java
+++
@@ -1,4 +0,0 @@
-package com.dsa.twodarrays;
-
-public class Basic {
-}
