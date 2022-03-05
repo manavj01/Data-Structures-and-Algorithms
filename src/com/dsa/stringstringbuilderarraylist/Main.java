ad1c5cfce7c4198b3e4e06099584513274f9643d
Manav Jain <manavj0120@gmail.com>
2022-03-05 22:32:03 +0530

    String Builder

--- src/com/dsa/stringstringbuilderarraylist/Main.java
+++ src/com/dsa/stringstringbuilderarraylist/Main.java
@@ -6,6 +6,25 @@ public class Main {
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
         String s = scn.nextLine();
-        PalindromicSubstrings.solution(s);
+        String t = scn.next();
+        /**
+         * Palindromic Substring
+         */
+//        PalindromicSubstrings.solution(s);
+
+        /**
+         * String Compression
+         */
+//        String s1 = StringCompression.compression1(s);
+//        System.out.println(s1);
+//        String s2 = StringCompression.compression2(s);
+//        System.out.println(s2);
+
+        /**
+         * Toggle Case
+         */
+
+        String s1 = ToggleCase.toggleCase(t);
+        System.out.println(s1);
     }
 }
---
+++ src/com/dsa/stringstringbuilderarraylist/ToggleCase.java
@@ -0,0 +1,11 @@
+package com.dsa.stringstringbuilderarraylist;
+
+public class ToggleCase {
+    public static String toggleCase(String str){
+        String s1="abc";
+
+        ;
+
+        return s1;
+    }
+}
