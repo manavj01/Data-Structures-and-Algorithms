da48a96daeb0a338b042afbc0a7907361e55b494
Manav Jain <67707554+manavj01@users.noreply.github.com>
2022-03-06 17:20:39 +0530

    LongestCommonSequence

---
+++ src/com/dsa/hashmaps/LongestCommonSequence.java
@@ -0,0 +1,46 @@
+package com.dsa.hashmaps;
+
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class LongestCommonSequence {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int[] arr = new int[n];
+
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = scn.nextInt();
+        }
+
+        HashMap<Integer, Boolean> map = new HashMap<>();
+        for (int val :
+                arr) {
+            map.put(val, true);
+        }
+
+        for (int val : arr) {
+            if (map.containsKey(val - 1)) {
+                map.put(val, false);
+            }
+        }
+        int msp = 0;
+        int ml = 0;
+        for (int val : arr) {
+            if (map.get(val)) {
+                int tl = 1;
+                int tsp = val;
+                while (map.containsKey(tsp + tl)) {
+                    tl++;
+                }
+                if (tl > ml) {
+                    msp = tsp;
+                    ml = tl;
+                }
+            }
+        }
+        for (int i = 0; i < ml; i++) {
+            System.out.println(msp + i);
+        }
+    }
+}
