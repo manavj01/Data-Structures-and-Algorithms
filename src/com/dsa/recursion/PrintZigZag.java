3382d5f15906911723d6dd82776b25f92f6fffee
Manav Jain <67707554+manavj01@users.noreply.github.com>
2022-03-10 00:37:44 +0530

    PrintZigZag recursion

---
+++ src/com/dsa/recursion/PrintZigZag.java
@@ -0,0 +1,23 @@
+package com.dsa.recursion;
+
+import java.util.Scanner;
+
+public class PrintZigZag {
+    public static void main(String[] args) throws Exception {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        scn.close();
+        pzz(n);
+    }
+
+    public static void pzz(int n){
+        if(n==0){
+            return;
+        }
+        System.out.print(n+ " ");
+        pzz(n-1);
+        System.out.print(n+ " ");
+        pzz(n-1);
+        System.out.print(n+ " ");
+    }
+}
