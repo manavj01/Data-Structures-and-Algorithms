04acedcdec5d20b94ac2a8dffb2317abf73349c5
Manav Jain <67707554+manavj01@users.noreply.github.com>
2022-03-09 22:35:06 +0530

    Factorial

---
+++ src/com/dsa/recursion/Factorial.java
@@ -0,0 +1,25 @@
+package com.dsa.recursion;
+
+import java.util.Scanner;
+
+public class Factorial {
+
+    public static int factorial(int n){
+        if(n==0){
+            return 1;
+        }
+        int fact = n;
+        int fnm1 = factorial(n-1);
+        fact = fact*fnm1;
+        return fact;
+    }
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n= scn.nextInt();
+        scn.close();
+
+        System.out.println(factorial(n));
+
+    }
+}
