5d565f966394222a197b6c79902daf01fa2d4102
Manav Jain <67707554+manavj01@users.noreply.github.com>
2022-03-10 23:35:30 +0530

    TowerOfHanoi recursion

---
+++ src/com/dsa/recursion/TowerOfHanoi.java
@@ -0,0 +1,23 @@
+package com.dsa.recursion;
+
+import java.util.Scanner;
+
+public class TowerOfHanoi {
+    public static void main(String[] args) throws Exception {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int t1d = scn.nextInt();
+        int t2d = scn.nextInt();
+        int t3d = scn.nextInt();
+        toh(n,t1d,t2d,t3d);
+    }
+
+    public static void toh(int n, int t1id, int t2id, int t3id){
+        if(n==0){
+            return;
+        }
+        toh(n-1,t1id,t3id,t2id); // will print the inst. to move n-1 disks from t1 to t3 using t2(*)
+        System.out.println(n+ "["+ t1id +"->" + t2id + "]");
+        toh(n-1,t3id,t2id,t1id);
+    }
+}
