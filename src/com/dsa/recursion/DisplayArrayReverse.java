6ceb708716652fdd023dc72d091deda6bbd1ac5e
Manav Jain <67707554+manavj01@users.noreply.github.com>
2022-03-11 23:39:21 +0530

    DisplayArrayReverse Recursion

---
+++ src/com/dsa/recursion/DisplayArrayReverse.java
@@ -0,0 +1,34 @@
+package com.dsa.recursion;
+import java.io.*;
+import java.util.*;
+
+public class DisplayArrayReverse {
+    public static void main(String[] args) throws Exception {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = scn.nextInt();
+        }
+        //a1
+       // displayArrReverse(arr,n);
+        //a2
+        displayArrReverse(arr,0);
+    }
+
+    public static void displayArrReverse(int[] arr, int idx) {
+        //a1
+//        if(idx==0){
+//            return;
+//        }
+//
+//        System.out.println(arr[idx-1]);
+//        displayArrReverse(arr,idx-1);
+
+        //a2
+        if(idx== arr.length) return;
+        displayArrReverse(arr,idx+1);
+        System.out.println(arr[idx]);
+
+    }
+}
