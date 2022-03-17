abc4b4f07fa68547c781c8255fe7edcd1d51281e
Manav Jain <67707554+manavj01@users.noreply.github.com>
2022-03-17 21:17:32 +0530

    PrintMazePathsWithJumps Recursion

---
+++ src/com/dsa/recursion/PrintMazePathsWithJumps.java
@@ -0,0 +1,40 @@
+package com.dsa.recursion;
+
+import java.util.Scanner;
+
+public class PrintMazePathsWithJumps {
+    public static void main(String[] args) throws Exception {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int m = scn.nextInt();
+
+        printMazePaths(1,1,n,m,"");
+    }
+
+    // sr - source row
+    // sc - source column
+    // dr - destination row
+    // dc - destination column
+    public static void printMazePaths(int sr, int sc, int dr, int dc, String psf) {
+
+        if(sc==dc && sr==dr){
+            System.out.println(psf);
+            return;
+        }
+
+        //for horizontal path
+        for (int ms=1; ms<=dc-sc; ms++){
+            printMazePaths(sr,sc+ms,dr,dc,psf+"h"+ms);
+        }
+        //for vertical path
+        for (int ms=1; ms<=dr-sr;ms++) {
+            printMazePaths(sr+ms,sc,dr,dc,psf+"v"+ms);
+        }
+        //for diagonal path
+
+        for (int ms=1; ms<= dr-sr && ms<=dc-sc; ms++) {
+            printMazePaths(sr+ms,sc+ms,dr,dc,psf+"d"+ms);
+        }
+    }
+
+}
