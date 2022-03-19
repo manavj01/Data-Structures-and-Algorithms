488729ff447cf1dafb3007facf9fc42fa25e6ee3
Manav Jain <67707554+manavj01@users.noreply.github.com>
2022-03-19 16:34:28 +0530

    TargetSUmSubsets Recursion

---
+++ src/com/dsa/recursion/TargetSumSubsets.java
@@ -0,0 +1,31 @@
+package com.dsa.recursion;
+
+import java.util.Scanner;
+
+public class TargetSumSubsets {
+
+    public static void main(String[] args) throws Exception {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int[] arr = new int[n];
+        for (int i =0; i<arr.length;i++){
+            arr[i] = scn.nextInt();
+        }
+        int target = scn.nextInt();
+        printTargetSumSubsets(arr,0,"",0,target);
+    }
+
+    // set is the subset
+    // sos is sum of subset
+    // tar is target
+    public static void printTargetSumSubsets(int[] arr, int idx, String set, int sos, int target) {
+        if(idx == arr.length){
+            if(sos == target){
+            System.out.println(set+".");
+            }
+            return;
+        }
+        printTargetSumSubsets(arr,idx+1,set+arr[idx]+", ",sos+ arr[idx],target);
+        printTargetSumSubsets(arr,idx+1,set,sos,target);
+    }
+}
