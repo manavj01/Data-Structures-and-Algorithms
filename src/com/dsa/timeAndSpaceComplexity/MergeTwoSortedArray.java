6f6ac175587dffcc49458b18dc779ab6475eaf05
Manav Jain <67707554+manavj01@users.noreply.github.com>
2022-04-21 17:57:25 +0530

    MergeTwoSortedArray

---
+++ src/com/dsa/timeAndSpaceComplexity/MergeTwoSortedArray.java
@@ -0,0 +1,56 @@
+package com.dsa.timeAndSpaceComplexity;
+
+import java.util.Scanner;
+
+public class MergeTwoSortedArray {
+    public static int[] mergeTwoSortedArrays(int[] a, int[] b){
+        int[] res = new int[a.length + b.length];
+        int i =0;
+        int j = 0;
+        int k = 0;
+
+        while (i<a.length && j< b.length){
+            if (a[i] < b[j]){
+                res[k] = a[i];
+                i++;
+                k++;
+            }else {
+                res[k] = b[j];
+                j++;
+                k++;
+            }
+        }
+        while (i < a.length){
+            res[k] = a[i];
+            i++;
+            k++;
+        }
+        while (j<b.length){
+            res[k] = b[j];
+            j++;
+            k++;
+        }
+        return res;
+    }
+
+    public static void print(int[] arr){
+        for (int j : arr) {
+            System.out.println(j);
+        }
+    }
+    public static void main(String[] args){
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int[] a = new int[n];
+        for(int i = 0 ; i < n; i++){
+            a[i] = scn.nextInt();
+        }
+        int m = scn.nextInt();
+        int[] b = new int[m];
+        for(int i = 0 ; i < m; i++){
+            b[i] = scn.nextInt();
+        }
+        int[] mergedArray = mergeTwoSortedArrays(a,b);
+        print(mergedArray);
+    }
+}
