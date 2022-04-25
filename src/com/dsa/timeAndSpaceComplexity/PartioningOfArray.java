42b4f0a63411e3cbc472bb46f34f4035efcce435
Manav Jain <67707554+manavj01@users.noreply.github.com>
2022-04-25 23:32:53 +0530

    PartioningOfArray

---
+++ src/com/dsa/timeAndSpaceComplexity/PartioningOfArray.java
@@ -0,0 +1,55 @@
+package com.dsa.timeAndSpaceComplexity;
+
+import java.util.Scanner;
+
+public class PartioningOfArray {
+
+    public static void partition(int[] arr, int pivot){
+
+            //0 to j-1 ->  <= pivot
+            //j to i-1 ->  > pivot
+            // i to end -> unknown
+
+            int i =0,j=0;
+
+            while (i< arr.length){
+                if (arr[i]>pivot){
+                    i++;
+                }else {
+                    swap(arr,i,j);
+                    i++;
+                    j++;
+                }
+            }
+
+        }
+
+        // used for swapping ith and jth elements of array
+        public static void swap(int[] arr, int i, int j) {
+            System.out.println("Swapping " + arr[i] + " and " + arr[j]);
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+        }
+
+        public static void print(int[] arr) {
+            for (int j : arr) {
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+
+        public static void main(String[] args) throws Exception {
+            Scanner scn = new Scanner(System.in);
+            int n = scn.nextInt();
+            int[] arr = new int[n];
+            for (int i = 0; i < n; i++) {
+                arr[i] = scn.nextInt();
+            }
+            int pivot = scn.nextInt();
+            partition(arr,pivot);
+            print(arr);
+        }
+
+    }
+
