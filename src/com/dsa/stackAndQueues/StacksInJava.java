4275ccce9862267ec6e8e27d99b21423b5f683f9
Manav Jain <67707554+manavj01@users.noreply.github.com>
2022-05-05 21:59:21 +0530

    StacksInJava

---
+++ src/com/dsa/stackAndQueues/StacksInJava.java
@@ -0,0 +1,29 @@
+package com.dsa.stackAndQueues;
+
+import java.util.Stack;
+
+public class StacksInJava {
+    public static void main(String[] args) {
+        Stack<Integer> st = new Stack<>();
+        st.push(10);
+        System.out.println(st +"->" + st.peek() + " " + st.size());
+        st.push(20);
+        System.out.println(st +"->" + st.peek() + " " + st.size());
+        st.push(30);
+        System.out.println(st +"->" + st.peek() + " " + st.size());
+        st.push(40);
+        System.out.println(st +"->" + st.peek() + " " + st.size());
+
+
+        st.pop();
+        System.out.println(st +"->" + st.peek() + " " + st.size());
+        st.pop();
+        System.out.println(st +"->" + st.peek() + " " + st.size());
+        st.pop();
+        System.out.println(st +"->" + st.peek() + " " + st.size());
+        st.pop();
+        System.out.println(st +"->"  + " " + st.size());
+
+
+    }
+}
