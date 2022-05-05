a1075d260b1cab1d215ce5a9b8363d4c0659b4b1
Manav Jain <67707554+manavj01@users.noreply.github.com>
2022-05-05 20:14:48 +0530

    Sort_01

--- src/com/dsa/timeAndSpaceComplexity/SortDates.java
+++ src/com/dsa/timeAndSpaceComplexity/SortDates.java
@@ -4,29 +4,30 @@ import java.util.Scanner;
 
 public class SortDates {
     public static void sortDates(String[] arr) {
-        countSort(arr,1000000,100,32); //days
-        countSort(arr,10000,100,13); //month
-        countSort(arr,1,10000,2501); //year
+        countSort(arr, 1000000, 100, 32); //days
+        countSort(arr, 10000, 100, 13); //month
+        countSort(arr, 1, 10000, 2501); //year
     }
 
-    public static void countSort(String[] arr,int div, int mod, int range) {
+    public static void countSort(String[] arr, int div, int mod, int range) {
         String[] ans = new String[arr.length];
         int[] farr = new int[range];
 
         for (int i = 0; i < arr.length; i++) {
-            farr[Integer.parseInt(arr[i],10)  / div % mod]++;
+            farr[Integer.parseInt(arr[i], 10) / div % mod]++;
         }
         for (int i = 1; i < farr.length; i++) {
             farr[i] += farr[i - 1];
         }
         for (int i = arr.length - 1; i >= 0; i--) {
-            int pos = farr[Integer.parseInt(arr[i],10) / div % mod] - 1;
+            int pos = farr[Integer.parseInt(arr[i], 10) / div % mod] - 1;
             ans[pos] = arr[i];
-            farr[Integer.parseInt(arr[i],10) / div % mod]--;
+            farr[Integer.parseInt(arr[i], 10) / div % mod]--;
         }
         for (int i = 0; i < ans.length; i++) {
             arr[i] = ans[i];
-        }    }
+        }
+    }
 
     public static void print(String[] arr) {
         for (int i = 0; i < arr.length; i++) {
