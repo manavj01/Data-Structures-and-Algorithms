b2d7a1c8fc941ae4d6021d2a201d3a9f667b3e09
Manav Jain <67707554+manavj01@users.noreply.github.com>
2022-05-05 20:57:10 +0530

    TargetSumPair

---
+++ src/com/dsa/timeAndSpaceComplexity/TargetSumPair.java
@@ -0,0 +1,37 @@
+package com.dsa.timeAndSpaceComplexity;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class TargetSumPair {
+    public static void targetSumPair(int[] arr, int target) {
+        Arrays.sort(arr);
+
+        int li=0;
+        int ri = arr.length-1;
+
+        while (li<=ri){
+            if (arr[li]+arr[ri]==target){
+                System.out.println(arr[li]+", "+arr[ri]);
+                li++;
+                ri--;
+            } else if (arr[li]+arr[ri]>target) {
+                ri--;
+            }else {
+                li++;
+            }
+        }
+
+    }
+
+    public static void main(String[] args) throws Exception {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = scn.nextInt();
+        }
+        int target = scn.nextInt();
+        targetSumPair(arr, target);
+    }
+}
