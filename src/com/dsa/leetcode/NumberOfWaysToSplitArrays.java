e6a4617297cd50151496080adba2116bb4c518fd
Manav Jain <67707554+manavj01@users.noreply.github.com>
2022-05-14 23:41:43 +0530

    SlidingWindowMaximum

---
+++ src/com/dsa/leetcode/NumberOfWaysToSplitArrays.java
@@ -0,0 +1,60 @@
+package com.dsa.leetcode;
+
+import java.util.Scanner;
+
+public class NumberOfWaysToSplitArrays {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int nums[] = new int[n];
+        for (int i = 0; i < n; i++) {
+            nums[i] = scn.nextInt();
+        }
+        int ans = waysToSplitArray(nums);
+        System.out.println(ans);
+    }
+
+    public static int waysToSplitArray(int[] nums) {
+        int valid_splits = 0;
+
+        int left_arr = nums[0];
+        int arrsum=0;
+        for (int i=1; i< nums.length; i++){
+            arrsum += nums[i];
+        }
+
+        if (left_arr>arrsum){
+            valid_splits++;
+        }
+
+        for (int pos = 1; pos< nums.length-2; pos++){
+            int nums_left[] = new int[pos];
+            int nums_right[] = new int[nums.length - pos];
+
+            for (int i = 0; i < nums.length; i++) {
+                if (i < pos) {
+                    nums_left[i] = nums[i];
+                } else {
+                    nums_right[i - pos] = nums[i];
+                }
+
+            }
+
+            int sum_left = 0;
+            for (int j = 0; j < nums_left.length; j++) {
+                sum_left += nums_left[j];
+            }
+            int sum_right = 0;
+            for (int j = 0; j < nums_left.length; j++) {
+                sum_right += nums_right[j];
+            }
+
+            if (sum_left > sum_right) {
+                valid_splits++;
+            }
+
+        }
+
+        return valid_splits;
+    }
+}
---
+++ src/com/dsa/stackAndQueues/SlidingWindowMaximum.java
@@ -0,0 +1,18 @@
+package com.dsa.stackAndQueues;
+
+import java.io.*;
+
+public class SlidingWindowMaximum {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        int[] a = new int[n];
+        for(int i = 0; i < n; i++){
+            a[i] = Integer.parseInt(br.readLine());
+        }
+        int k = Integer.parseInt(br.readLine());
+
+        // code
+    }
+}
