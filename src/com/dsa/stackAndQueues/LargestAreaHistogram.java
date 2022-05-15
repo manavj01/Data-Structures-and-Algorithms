85b3153550cb26203dd2945de2c2d55b796c8615
Manav Jain <67707554+manavj01@users.noreply.github.com>
2022-05-15 19:37:19 +0530

    InfixConversion

--- src/com/dsa/stackAndQueues/LargestAreaHistogram.java
+++ src/com/dsa/stackAndQueues/LargestAreaHistogram.java
@@ -16,7 +16,7 @@ public class LargestAreaHistogram {
         int rb[] = new int[arr.length];  //nse index on the right
 
         Stack<Integer> st = new Stack<>();
-        st.push(arr.length -1);
+        st.push(arr.length - 1);
         rb[arr.length - 1] = arr.length;
 
         for (int i = arr.length - 2; i >= 0; i--) {
@@ -24,7 +24,7 @@ public class LargestAreaHistogram {
                 st.pop();
             }
 
-            if (st.size() == 0){
+            if (st.size() == 0) {
                 rb[i] = arr.length;
             } else {
                 rb[i] = st.peek();
@@ -38,13 +38,13 @@ public class LargestAreaHistogram {
         st.push(0);
         lb[0] = -1;
 
-        for (int i=1; i< arr.length;i++){
-            while (st.size()>0 && arr[i]<= arr[st.peek()]){
+        for (int i = 1; i < arr.length; i++) {
+            while (st.size() > 0 && arr[i] <= arr[st.peek()]) {
                 st.pop();
             }
-            if (st.size() == 0){
+            if (st.size() == 0) {
                 lb[i] = -1;
-            }else {
+            } else {
                 lb[i] = st.peek();
             }
             st.push(i);
--- src/com/dsa/stackAndQueues/StockSpan.java
+++ src/com/dsa/stackAndQueues/StockSpan.java
@@ -33,14 +33,14 @@ public class StockSpan {
         st.push(0);
         span[0] = 1;
 
-        for (int i =1; i< arr.length; i++){
-            while (st.size()>0 && arr[i] > arr[st.peek()]){
+        for (int i = 1; i < arr.length; i++) {
+            while (st.size() > 0 && arr[i] > arr[st.peek()]) {
                 st.pop();
             }
-            if (st.size()  == 0){
-                span[i] = i+1;
-            }else {
-                span[i] = i- st.peek();
+            if (st.size() == 0) {
+                span[i] = i + 1;
+            } else {
+                span[i] = i - st.peek();
             }
             st.push(i);
         }
