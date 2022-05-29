bb97321e600ed92d926d6a1d856b76f44a545dda
Manav Jain <67707554+manavj01@users.noreply.github.com>
2022-05-29 18:01:18 +0530

    RearrangeCharactersToMakeTargetString

---
+++ src/com/dsa/leetcode/RearrangeCharactersToMakeTargetString.java
@@ -0,0 +1,36 @@
+package com.dsa.leetcode;
+
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class RearrangeCharactersToMakeTargetString {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        String s = "abbaccaddaeea"; //"iloveleetcodingdoc";//scn.nextLine();
+        String target = "aaaaab";//"code";//scn.nextLine();
+        int ans = rearrangeCharacters(s, target);
+        System.out.println(ans);
+    }
+
+    public static int rearrangeCharacters(String s, String target) {
+        HashMap<Character, Integer> fmap = getMap(s);
+        HashMap<Character, Integer> tmap = getMap(target);
+        int min = Integer.MAX_VALUE;
+        for (char ch : target.toCharArray()) {
+            min = Math.min(min, fmap.get(ch) / tmap.get(ch));
+        }
+        return min;
+    }
+
+    public static HashMap<Character, Integer> getMap(String str) {
+        HashMap<Character, Integer> map = new HashMap<>();
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+            map.put(ch, map.containsKey(ch) ? map.get(ch) + 1 : 1);
+        }
+        return map;
+    }
+}
+
+//ilovecodingonleetcode
+//code
