be7ad94b3726faee2c0873cf51dbd68ba7d3a091
Manav Jain <67707554+manavj01@users.noreply.github.com>
2022-05-29 16:10:58 +0530

    CheckIfNumberHasEaqualDigitCountAndDigitValue

---
+++ src/com/dsa/leetcode/CheckIfNumberHasEaqualDigitCountAndDigitValue.java
@@ -0,0 +1,36 @@
+package com.dsa.leetcode;
+
+import java.util.Scanner;
+
+public class CheckIfNumberHasEaqualDigitCountAndDigitValue {
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        String num = scn.nextLine();
+        boolean soln = digitCount(num);
+        System.out.println(soln);
+    }
+    public static boolean digitCount(String num) {
+        int[] arr = new int[num.length()];
+        for(int i =0 ; i<num.length(); i++){
+            char ch = num.charAt(i);
+            int dig = ch-'0';
+            arr[i] = dig;
+        }
+        for (int i = 0 ; i< arr.length; i++){
+            int val = arr[i];
+
+            int counter = 0;
+            for (int j =0 ; j< arr.length; j++){
+                if (i == arr[j]){
+                    counter++;
+                }
+            }
+            if (counter != val){
+                return false;
+            }
+        }
+
+       return true;
+    }
+}
