bfba6d5a9cdede964b84fc37541a883c4324a3eb
Manav Jain <67707554+manavj01@users.noreply.github.com>
2022-06-01 22:19:34 +0530

    LinkedListFunctions

---
+++ src/com/dsa/linkedlist/LinkedList.java
@@ -0,0 +1,224 @@
+package com.dsa.linkedlist;
+
+public class LinkedList {
+
+    public static class Node {
+        int data;
+        Node next;
+    }
+
+    Node head;
+    Node tail;
+    int size;
+
+    void addLast(int val) {
+        Node temp = new Node();
+        temp.data = val;
+        temp.next = null;
+
+        if (size == 0) {
+            head = tail = temp;
+        } else {
+            tail.next = temp;
+            tail = temp;
+        }
+
+        size++;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public void display() {
+//        for (Node temp = head; temp != null; temp = temp.next) {
+//            System.out.print(temp.data + " ");
+//        }
+//        System.out.println();
+        Node temp = new Node();
+        temp = head;
+        while (temp.next != null) {
+            System.out.print(temp.data + " ");
+            temp = temp.next;
+        }
+        System.out.print(temp.data);
+        System.out.println();
+
+    }
+
+    public void removeFirst() {
+        if (size == 0) {
+            System.out.println("List is empty");
+        } else if (size == 1) {
+            head = tail = null;
+            size = 0;
+        } else {
+//            Node temp = new Node();
+//            temp = head;
+//            temp = temp.next;
+//            head = temp;
+//            size--;
+            head = head.next;
+            size--;
+        }
+    }
+
+    public int getFirst() {
+        if (size == 0) {
+            System.out.println("List is empty");
+            return -1;
+        } else {
+            return head.data;
+        }
+    }
+
+    public int getLast() {
+        if (size == 0) {
+            System.out.println("List is empty");
+            return -1;
+        } else {
+            return tail.data;
+        }
+    }
+
+    public int getAt(int idx) {
+        if (size == 0) {
+            System.out.println("List is empty");
+            return -1;
+        } else if (idx < 0 || idx >= size) {
+            System.out.println("Invalid arguments");
+            return -1;
+        } else {
+            Node temp = head;
+            for (int i = 0; i < idx; i++) {
+                temp = temp.next;
+            }
+            return temp.data;
+
+//            Node temp = new Node();
+//            temp = head;
+//            int i =0;
+//            while(i != idx){
+//                temp= temp.next;
+//                i++;
+//            }
+//            return temp.data;
+        }
+    }
+
+    public void addFirst(int val) {
+        Node temp = new Node();
+        temp.data = val;
+        temp.next = head;
+        head = temp;
+
+        if (size == 0) {
+            tail = temp;
+        }
+
+        size++;
+    }
+
+    public void addAt(int idx, int val) {
+        if (idx < 0 || idx > size) {
+            System.out.println("Invalid arguments");
+        } else if (idx == 0) {
+            addFirst(val);
+        } else if (idx == size) {
+            addLast(val);
+        } else {
+            Node node = new Node();
+            node.data = val;
+
+            Node temp = head;
+            for (int i = 0; i < idx - 1; i++) {
+                temp = temp.next;
+            }
+            node.next = temp.next;
+
+            temp.next = node;
+            size++;
+        }
+    }
+
+    public void removeLast() {
+        if (size == 0) {
+            System.out.println("List is empty");
+        } else if (size == 1) {
+            head = tail = null;
+            size = 0;
+        } else {
+            Node temp = head;
+            for (int i = 0; i < size - 2; i++) {
+                temp = temp.next;
+            }
+
+            tail = temp;
+            tail.next = null;
+            size--;
+        }
+    }
+
+    public void removeAt(int idx) {
+        if (idx < 0 || idx >= size) {
+            System.out.println("Invalid arguments");
+        } else if (idx == 0) {
+            removeFirst();
+        } else if (idx == size - 1) {
+            removeLast();
+        } else {
+            Node temp = head;
+            for (int i = 0; i < idx - 1; i++) {
+                temp = temp.next;
+            }
+
+            temp.next = temp.next.next;
+            size--;
+        }
+    }
+
+    private Node getNodeAt(int idx) {
+        Node temp = head;
+        for (int i = 0; i < idx; i++) {
+            temp = temp.next;
+        }
+        return temp;
+    }
+
+    public void reverseDI() {
+        int li = 0;
+        int ri = size - 1;
+        while (li < ri) {
+            Node left = getNodeAt(li);
+            Node right = getNodeAt(ri);
+
+            int temp = left.data;
+            left.data = right.data;
+            right.data = temp;
+
+            li++;
+            ri--;
+        }
+    }
+
+    public void reversePI() {
+        // write your code here
+        Node prev = null;
+        Node current = head;
+
+        while (current != null) {
+            Node ocn = current.next;
+
+
+            current.next = prev;
+
+            prev = current;
+            current = ocn;
+        }
+
+        Node temp = head;
+        head = tail;
+        tail = temp;
+
+    }
+}
