7af31dec88398be8a9d505224ac13726b0da6dc5
Manav Jain <manavj0120@gmail.com>
2022-06-10 20:23:44 +0530

    TilingWithDominoes

---
+++ src/com/dsa/dynamicProgramming/TilingWithDominoes.java
@@ -0,0 +1,21 @@
+package com.dsa.dynamicProgramming;
+
+import java.util.Scanner;
+
+public class TilingWithDominoes {
+    public static void main(String[] args) {
+
+        Scanner scn = new Scanner(System.in);
+
+        int n = scn.nextInt();
+
+        int[] dp = new int[n + 1];
+        dp[1] = 1;
+        dp[2] = 2;
+
+        for (int i = 3; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+        System.out.println(dp[n]);
+    }
+}
