c816e6feb26d85d499059c352c95abeef3b70f7d
Manav Jain <manavj0120@gmail.com>
2022-06-10 20:23:34 +0530

    PaintFence

---
+++ src/com/dsa/dynamicProgramming/PaintFence.java
@@ -0,0 +1,27 @@
+package com.dsa.dynamicProgramming;
+
+import java.util.Scanner;
+
+public class PaintFence {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int k = scn.nextInt();
+
+        int same = k;
+        int diff = k * (k-1);
+        int total = same + diff;
+        for (int i = 3; i <= n; i++) {
+            int nsame = diff;
+           int  ndiff = total * (k -1);
+            int ntotal = nsame + ndiff;
+
+            same = nsame;
+            diff = ndiff;
+            total = ntotal;
+        }
+        System.out.println(total);
+
+
+    }
+}
