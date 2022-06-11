43408d5e89f5ee87756518516b142f07a4e89674
Manav Jain <manavj0120@gmail.com>
2022-06-11 19:15:33 +0530

    TilingWithM1Tiles

---
+++ src/com/dsa/dynamicProgramming/TilingWithM1Tiles.java
@@ -0,0 +1,25 @@
+package com.dsa.dynamicProgramming;
+
+import java.util.Scanner;
+
+public class TilingWithM1Tiles {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+
+        int n = scn.nextInt();
+        int m = scn.nextInt();
+
+        int[] dp = new int[n + 1];
+
+        for (int i = 1; i <= n; i++) {
+            if (i < m) {
+                dp[i] = 1;
+            } else if (i == m) {
+                dp[i] = 2;
+            } else {
+                dp[i] = dp[i - 1] + dp[i - m];
+            }
+        }
+        System.out.println(dp[n]);
+    }
+}
