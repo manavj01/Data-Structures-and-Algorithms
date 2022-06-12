d512bee5c33e9c07300273c6459dd1f6e7311aaf
Manav Jain <manavj0120@gmail.com>
2022-06-12 13:15:07 +0530

    CalculateAmountPaidInTaxes

--- src/com/dsa/leetcode/CalculateAmountPaidInTaxes.java
+++ src/com/dsa/leetcode/CalculateAmountPaidInTaxes.java
@@ -4,34 +4,26 @@ import java.util.Scanner;
 
 public class CalculateAmountPaidInTaxes {
     public static void main(String[] args) {
-        Scanner scn = new Scanner(System.in);
-        int n = scn.nextInt();
         int[][] brackets = {{3, 50}, {7, 10}, {12, 25}};
-//                new int[n][2];
-//        for (int i = 0 ; i < brackets.length; i++){
-//            for (int j = 0 ; j< 2; j++){
-//                brackets[i][j] = scn.nextInt();
-//            }
-//        }
-        int income = 10; //scn.nextInt();
-        double res = calculateTax(brackets, income);
-        System.out.println(res);
+        int income = 10;
+        System.out.println(calculateTax(brackets, income));
 
     }
 
     public static double calculateTax(int[][] brackets, int income) {
         if (income == 0) return 0.0;
         double tax = 0.0;
-        int prev =0;
-        for(int[] bracket : brackets){
-            int upper = bracket[0] ;
+        int prev = 0;
+        for (int[] bracket : brackets) {
+            int upper = bracket[0];
             int percent = bracket[1];
-            if(income >= upper){
-                tax += (upper-prev) * percent /100d;
+            if (income >= upper) {
+                tax += (upper - prev) * percent / 100d;
                 prev = upper;
-            }else{
-                tax += (income - prev) * percent /100d;
+            } else {
+                tax += (income - prev) * percent / 100d;
                 return tax;
+                
             }
         }
 
