6f2c93158ea26e056ddd791f950ec6a32768957e
Manav Jain <manavj0120@gmail.com>
2022-06-16 19:40:37 +0530

    LongestPalindrome

---
+++ src/com/dsa/leetcode/LongestPalindrome.java
@@ -0,0 +1,28 @@
+package com.dsa.leetcode;
+
+public class LongestPalindrome {
+    public static void main(String[] args) {
+        String str = "babad";
+        String res = longestPalindrome(str);
+        System.out.println(res);
+    }
+    public static String longestPalindrome(String s){
+
+        int n = s.length();
+        String res = null;
+
+        boolean[][] dp = new boolean[n][n];
+
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = i; j < n; j++) {
+                dp[i][j] = s.charAt(i) == s.charAt(j) && (j - i < 3 || dp[i + 1][j - 1]);
+
+                if (dp[i][j] && (res == null || j - i + 1 > res.length())) {
+                    res = s.substring(i, j + 1);
+                }
+            }
+        }
+
+        return res;
+    }
+}
