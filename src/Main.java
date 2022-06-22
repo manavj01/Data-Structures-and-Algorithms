3a8b561d0617792fa65fdf5212682f7459413668
Manav Jain <manavj0120@gmail.com>
2022-06-22 17:34:45 +0530

    testing

--- src/Main.java
+++ src/Main.java
@@ -4,15 +4,62 @@ import com.dsa.utility.Util;
 import java.util.ArrayList;
 import java.util.Scanner;
 
+class Dog {
+    String name;
+
+    public Dog(String name) {
+        this.name = name;
+    }
+}
+
 public class Main {
-
     public static void main(String[] args) {
-//        Scanner scn = new Scanner(System.in);
-//
-//        int[][] arr = Util.getInputInTwoDArr(scn);
-//        int searchKeyword = scn.nextInt();
-//        SearchInA2dArray.binarySearch(arr, searchKeyword);
-//        scn.close();
+        Integer sum = 0;
+        foo(sum);
+        System.out.println(sum == 0);
+    }
 
+    public static void foo(Integer s) {
+        System.out.println(s);
+        s = 100;
+    }
+
+
+//    public static void main(String[] args) {
+////        Scanner scn = new Scanner(System.in);
+////
+////        int[][] arr = Util.getInputInTwoDArr(scn);
+////        int searchKeyword = scn.nextInt();
+////        SearchInA2dArray.binarySearch(arr, searchKeyword);
+////        scn.close();
+////        int hi =15,lo =10,s=0;
+////
+////         sum(hi,lo,s);
+////        System.out.println(s);
+//
+//        Dog dog = new Dog("Max");
+//        Dog newDog = dog;
+//
+//        dog = foo(newDog);
+//        System.out.println(newDog.name.equals("Max"));
+//        System.out.println(newDog.name.equals("Manav"));
+//        System.out.println(newDog.name.equals("Random"));
+//        System.out.println(newDog == dog);
+//        System.out.println(dog.name.equals("Manav"));
+//    }
+//
+//    public static Dog foo(  Dog d) {
+//        System.out.println(d.name.equals("Max"));
+//
+//        d.name = "Manav";
+//        System.out.println(d.name.equals("Manav"));
+//
+//        d =  new Dog("Random"); // 733
+//        return d;
+//    }
+
+    public static void sum(int hi, int lo, int s) {
+
+        s = lo + hi;
     }
 }
