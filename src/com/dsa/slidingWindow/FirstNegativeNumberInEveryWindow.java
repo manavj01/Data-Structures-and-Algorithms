53b317f5589775cc94dac3907e2db6d37bfbc9fe
Manav Jain <manavj0120@gmail.com>
2022-07-06 23:10:49 +0530

    FirstNegativeNumberInEveryWindow

--- src/com/dsa/slidingWindow/FirstNegativeNumberInEveryWindow.java
+++ src/com/dsa/slidingWindow/FirstNegativeNumberInEveryWindow.java
@@ -1,7 +1,6 @@
 package com.dsa.slidingWindow;
 
-import java.util.Arrays;
-import java.util.Scanner;
+import java.util.*;
 
 public class FirstNegativeNumberInEveryWindow {
     public static void main(String[] args) {
@@ -14,8 +13,43 @@ public class FirstNegativeNumberInEveryWindow {
         System.out.println(Arrays.toString(printFirstNegativeInteger(A, N, K)));
     }
 
-    public static long[] printFirstNegativeInteger(long A[], int N, int K) {
+    public static long[] printFirstNegativeInteger(long[] A, int N, int K) {
+        int i = 0;
+        int j = 0;
+        int z = 0;
+        ArrayList<Integer> list = new ArrayList<>();
+        long[] res = new long[N - K + 1];
 
-        return new long[0];
+        while (j < N) {
+            if (A[j] < 0) {
+                list.add(j);
+            }
+
+            if (j - i + 1 < K) {
+                j++;
+            } else if (j - i + 1 == K) {
+                if (list.size() == 0) {
+                    res[z++] = 0;
+                } else if (list.size() > 0) {
+                    res[z++] = A[list.get(0)];
+                }
+                i++;
+                j++;
+                if (list.size() > 0 && list.get(0) < i) {
+                    list.remove(list.get(0));
+                }
+            }
+        }
+
+        return res;
     }
 }
+
+// input
+//5
+//-8 2 3 -6 10
+//2
+
+//8
+//12 -1 -7 8 -15 30 16 28
+//3
