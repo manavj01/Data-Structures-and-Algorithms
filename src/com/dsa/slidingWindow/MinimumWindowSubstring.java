2ed36deeb6073227c2b0857cea7e8f48f85e10db
Manav Jain <manavj0120@gmail.com>
2022-07-16 20:12:43 +0530

    MinimumWindowSubstring

---
+++ src/com/dsa/slidingWindow/MinimumWindowSubstring.java
@@ -0,0 +1,36 @@
+package com.dsa.slidingWindow;
+
+public class MinimumWindowSubstring {
+    public static void main(String[] args) {
+        String s = "ADOBECODEBANC", t = "ABC";
+        System.out.println(minWindow(s, t));
+    }
+
+    public static String minWindow(String s, String t) {
+        int[] map = new int[128];
+
+        for (int i = 0; i < t.length(); i++) map[t.charAt(i)]++;
+
+        int start = 0, end = 0, minStart = 0, minLen = Integer.MAX_VALUE, counter = t.length();
+
+        while (end < s.length()) {
+            final char c1 = s.charAt(end);
+            if (map[c1] > 0) counter--;
+            map[c1]--;
+            end++;
+
+            while (counter == 0) {
+                if (minLen > end - start) {
+                    minLen = end - start;
+                    minStart = start;
+                }
+                final char c2 = s.charAt(start);
+                map[c2]++;
+                if (map[c2] > 0) counter++;
+                start++;
+            }
+        }
+
+        return minLen == Integer.MAX_VALUE ? "" : s.substring(minStart, minStart + minLen);
+    }
+}
