097d6c5eb0c20e7ce0ac01b55ad5fa505bc35146
Manav Jain <manavj0120@gmail.com>
2022-07-28 17:18:17 +0530

    UniquePaths

--- src/com/dsa/striver30daysSheet/arrays3/UniquePaths.java
+++ src/com/dsa/striver30daysSheet/arrays3/UniquePaths.java
@@ -3,7 +3,7 @@ package com.dsa.striver30daysSheet.arrays3;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Scanner;
+import java.util.Arrays;
 
 public class UniquePaths {
     public static void main(String[] args) throws IOException {
@@ -14,44 +14,80 @@ public class UniquePaths {
         System.out.println(uniquePaths(n, m));
     }
 
+    public static int uniquePaths(int n, int m) {
+        int N = n + m - 2;
+        int r = n - 1;
+//        or
+//        int r = m-1;
+        double res = 1;
+        for (int i = 1; i <= r; i++) {
+            res = res * (N - r + i) / i;
+        }
+        return (int) res;
+    }
+
     static int c = 0;
 
     public static int uniquePaths1(int n, int m) {
         boolean[][] arr = new boolean[n][m];
         int sr = 0, sc = 0;
-        getPaths(arr, sr, sc);
+        uniquePaths1Helper(arr, sr, sc);
         return c;
     }
 
-    public static void getPaths(boolean[][] arr, int sr, int sc) {
-        if (sr==arr.length||sc == arr[0].length || arr[sr][sc]) {
+    public static void uniquePaths1Helper(boolean[][] arr, int sr, int sc) {
+        if (sr == arr.length || sc == arr[0].length || arr[sr][sc]) {
             return;
         }
 
-        if (sc == arr[0].length-1 && sr == arr.length-1) {
+        if (sc == arr[0].length - 1 && sr == arr.length - 1) {
             c++;
             return;
         }
 
         arr[sr][sc] = true;
-        getPaths(arr, sr, sc + 1);
-        getPaths(arr, sr + 1, sc);
+        uniquePaths1Helper(arr, sr, sc + 1);
+        uniquePaths1Helper(arr, sr + 1, sc);
         arr[sr][sc] = false;
     }
 
-    public static int uniquePaths(int n, int m){
-        int i=0,j=0;
-        int ans = countPaths(i,j,n,m);
-        return ans;
+    public static int uniquePaths2(int n, int m) {
+        int i = 0, j = 0;
+        return uniquePaths2Helper(i, j, n, m);
     }
-    public static int countPaths(int i,int j, int n , int m){
-        if (i == (n-1) && j == (m-1)){
+
+    public static int uniquePaths2Helper(int i, int j, int n, int m) {
+        if (i == (n - 1) && j == (m - 1)) {
             return 1;
         }
-        if (i>= n || j>= m){
+        if (i >= n || j >= m) {
             return 0;
-        }else return countPaths(i+1,j,n,m)+countPaths(i,j+1,n,m);
+        } else return uniquePaths2Helper(i + 1, j, n, m) + uniquePaths2Helper(i, j + 1, n, m);
 
     }
 
+    public static int uniquePaths3(int n, int m) {
+        int[][] dp = new int[n][m];
+        for (int[] a : dp) {
+            Arrays.fill(a, -1);
+        }
+
+        return uniquePaths3Helper(0, 0, n, m, dp);
+    }
+
+    public static int uniquePaths3Helper(int i, int j, int n, int m, int[][] dp) {
+        if (i == (n - 1) && j == (m - 1)) {
+            return 1;
+        }
+
+        if (i >= n || j >= m) {
+            return 0;
+        }
+
+        if (dp[i][j] != -1) {
+            return dp[i][j];
+        } else {
+            return dp[i][j] = uniquePaths3Helper(i + 1, j, n, m, dp) + uniquePaths3Helper(i, j + 1, n, m, dp);
+        }
+    }
 }
