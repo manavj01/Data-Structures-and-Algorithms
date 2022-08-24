e19a726c725af82165b641f49f228c0e913d2812
Manav Jain <manavj0120@gmail.com>
2022-08-24 20:43:42 +0530

    BubbleSort

---
+++ src/com/dsa/interviewQuestions/BubbleSort.java
@@ -0,0 +1,26 @@
+package com.dsa.interviewQuestions;
+
+import java.util.Arrays;
+
+public class BubbleSort {
+    public static void main(String[] args) {
+
+        int[] arr = new int[]{12, 31, 25, 8, 32, 17, 40, 42};
+        bubbleSort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    public static void bubbleSort(int[] arr) {
+
+        for (int i = arr.length - 1; i >= 0; i--) {
+            for (int j = 0; j < i; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    arr[j] = arr[j] ^ arr[j + 1] ^ (arr[j + 1] = arr[j]); //swapping
+                }
+
+            }
+        }
+    }
+
+
+}
