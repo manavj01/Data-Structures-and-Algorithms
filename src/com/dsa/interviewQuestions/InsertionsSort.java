2033965ef4a54557275a5ec32464d0f50f4e4b4b
Manav Jain <manavj0120@gmail.com>
2022-08-25 23:53:55 +0530

    InsertionsSort

---
+++ src/com/dsa/interviewQuestions/InsertionsSort.java
@@ -0,0 +1,23 @@
+package com.dsa.interviewQuestions;
+
+import java.util.Arrays;
+
+public class InsertionsSort {
+    public static void main(String[] args) {
+        int[] arr = new int[]{2, 9, 5, 1, 3};
+        insertionSort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    public static void insertionSort(int[] arr) {
+        for (int i = 1; i < arr.length; i++) {
+            for (int j = i - 1; j >= 0; j--) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+    }
+}
