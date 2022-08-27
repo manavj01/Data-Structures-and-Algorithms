65fca69a7df6bda470e6cc61a10b9e619f9a4d7c
Manav Jain <manavj0120@gmail.com>
2022-08-27 23:56:27 +0530

    SelectionSort

---
+++ src/com/dsa/interviewQuestions/SelectionSort.java
@@ -0,0 +1,31 @@
+package com.dsa.interviewQuestions;
+
+import java.util.Arrays;
+import java.util.Map;
+
+public class SelectionSort {
+    public static void main(String[] args) {
+
+        int[] arr = new int[]{12, 31, 25, 8, 32, 17, 40, 42};
+        selectionSort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    public static void selectionSort(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            int min = i;
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[min] > arr[j]) {
+                    min = j;
+                }
+            }
+            swap(arr, i, min);
+        }
+    }
+
+    public static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+}
