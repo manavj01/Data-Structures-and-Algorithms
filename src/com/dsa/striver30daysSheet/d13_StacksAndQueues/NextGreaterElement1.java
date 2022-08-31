9d5f960a601a237b88c6f101e744f7de70c264d7
Manav Jain <manavj0120@gmail.com>
2022-08-31 15:54:51 +0530

    NextGreaterElement1

---
+++ src/com/dsa/striver30daysSheet/d13_StacksAndQueues/NextGreaterElement1.java
@@ -0,0 +1,34 @@
+package com.dsa.striver30daysSheet.d13_StacksAndQueues;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Stack;
+
+public class NextGreaterElement1 {
+    public static void main(String[] args) {
+        int[] nums1 = new int[]{4, 1, 2};
+        int[] nums2 = new int[]{1, 3, 4, 2};
+
+        System.out.println(Arrays.toString(nextGreaterElement(nums1, nums2)));
+    }
+
+    public static int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        Map<Integer, Integer> map = new HashMap<>();
+        Stack<Integer> st = new Stack<>();
+
+        for (int num : nums2) {
+            while (!st.isEmpty() && num > st.peek()) {
+                map.put(st.pop(), num);
+            }
+            st.push(num);
+        }
+
+        for (int i = 0; i < nums1.length; i++) {
+            nums1[i] = map.getOrDefault(nums1[i], -1);
+        }
+
+        System.out.println(map);
+        return nums1;
+    }
+}
