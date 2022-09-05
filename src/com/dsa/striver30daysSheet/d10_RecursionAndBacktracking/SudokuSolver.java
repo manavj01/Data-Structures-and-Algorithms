32b37649f26ac3f5ffe8b0ab6f4229ad163fa6a6
Manav Jain <manavj0120@gmail.com>
2022-09-05 18:51:30 +0530

    SudokuSolver

---
+++ src/com/dsa/striver30daysSheet/d10_RecursionAndBacktracking/SudokuSolver.java
@@ -0,0 +1,64 @@
+package com.dsa.striver30daysSheet.d10_RecursionAndBacktracking;
+
+import java.util.Arrays;
+
+public class SudokuSolver {
+    public static void main(String[] args) {
+        char[][] board = new char[][]{
+                {'5', '3', '.', '.', '7', '.', '.', '.', '.'},
+                {'6', '.', '.', '1', '9', '5', '.', '.', '.'},
+                {'.', '9', '8', '.', '.', '.', '.', '6', '.'},
+                {'8', '.', '.', '.', '6', '.', '.', '.', '3'},
+                {'4', '.', '.', '8', '.', '3', '.', '.', '1'},
+                {'7', '.', '.', '.', '2', '.', '.', '.', '6'},
+                {'.', '6', '.', '.', '.', '.', '2', '8', '.'},
+                {'.', '.', '.', '4', '1', '9', '.', '.', '5'},
+                {'.', '.', '.', '.', '8', '.', '.', '7', '9'}
+        };
+        new SudokuSolver().solveSudoku(board);
+        for (char[] c : board) {
+            System.out.println(Arrays.toString(c));
+        }
+    }
+
+    public void solveSudoku(char[][] board) {
+        solve(board);
+    }
+
+    boolean solve(char[][] board) {
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[0].length; j++) {
+                if (board[i][j] == '.') {
+                    for (char c = '1'; c <= '9'; c++) {
+                        if (isValid(board, i, j, c)) {
+                            board[i][j] = c;
+
+                            if (solve(board)) {
+                                return true;
+                            } else {
+                                board[i][j] = '.';
+                            }
+                        }
+                    }
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    public boolean isValid(char[][] board, int row, int col, char c) {
+
+        for (int i = 0; i < 9; i++) {
+
+            if (board[i][col] == c) return false;
+
+            if (board[row][i] == c) return false;
+
+            if (board[3 * (row / 3) + i / 3][3 * (col / 3) + i % 3] == c)
+                return false;
+        }
+
+        return true;
+    }
+}
